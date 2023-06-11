c7567e3d6820860b9af441fa610a05872d136180
Goncalo Correia <gonkbsd@gmail.com>
2023-06-11 15:31:15 +0100

    Config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        node : true
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        ecmaVersion: 12,
+        requireConfigFile: false,
+    },
+    plugins: [
+        'vue'
+    ],
+    rules: {
+        'no-unused-vars': 'off'
+    }
+}
